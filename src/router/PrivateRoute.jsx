80da9b105509728f5ef93bc141549251e59067a1
foysalcodeio <md.foysalahammedforhad@gmail.com>
2025-12-20 23:39:29 +0600

    updated private router

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,16 +1,23 @@
-import { Navigate } from "react-router";
+import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
 
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useAuth();
 
+    const location = useLocation();
+    console.log(location);
+
     if (loading) {
         // return <progress className="progress w-56"></progress>
         return <span className="loading loading-spinner loading-xl"></span>
     }
     if (!user) {
-        return <Navigate to="/login"></Navigate>
+        return <Navigate
+            to="/login"
+            state={{ from: location }}
+            replace
+        />
     }
     return children;
 };
